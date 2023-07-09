fbbe3a8d255b76e1763dd6a8d7dda87c7c7f35b4
ShreyasHakke1809 <shreyashakke5858@gmail.com>
2023-07-09 12:04:19 +0530

    Adding Line Comparison Program

---
+++ LineComparisonProgram/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LineComparisonProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Line Comparison Program");
+        }
+    }
+}
